3598d9d34db8eef933b2c95c549a3afb787dfd2b
EuAliciane <98620470+EuAliciane@users.noreply.github.com>
2025-02-04 20:44:26 -0300

    Corrigido problema de segurança com SQL Injection

--- cadastro.php
+++ cadastro.php
@@ -1,35 +1,38 @@
 <body>
 <?php
-if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['cadastrar'])) {
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['cadastrar'])) {
     $nome = $_POST['nome'];
     $idade = $_POST['idade'];
     $sexo = $_POST['sexo'];
     $contato = $_POST['contato'];
 
-$servername = "localhost";
-$banco = "cadastro_de_usuario";
-$user = "root";
-$password = "";
+    $servername = "localhost";
+    $banco = "cadastro_de_usuario";
+    $user = "root";
+    $password = "";
 
-$conn = mysqli_connect($servername,  $user, $password, $banco);
+    $conn = mysqli_connect($servername, $user, $password, $banco);
 
-if(!$conn) {
-    die("Conexão falhou. " . mysqli_connect_error());
-}
-
-$sql = "INSERT INTO cadastros(nome, idade, sexo, contato) VALUES ('$nome', '$idade', '$sexo', '$contato')";
-
-if (mysqli_query($conn, $sql)) {
-echo "Você foi cadastrado com sucesso.";
-    } else {
-        echo "Erro: " . mysqli_error($conn);
+    if (!$conn) {
+        die("Conexão falhou: " . mysqli_connect_error());
     }
 
+    $stmt = $conn->prepare("INSERT INTO cadastros (nome, idade, sexo, contato) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("siss", $nome, $idade, $sexo, $contato);
+
+    if ($stmt->execute()) {
+        echo "Você foi cadastrado com sucesso.";
+    } else {
+        echo "Erro: " . $stmt->error;
+    }
+
+    $stmt->close();
     mysqli_close($conn);
 }
 ?>
- <div>
+<div>
     <br/><a href="javascript:history.go(-1)" class="botao">Voltar</a>
 </div>
+
 </body>
 </html>
